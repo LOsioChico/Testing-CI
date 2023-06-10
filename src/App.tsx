9b56433c4fcf6aa884cd55c000c4e25160bf7fdb
Luis Osio Chico <72286405+LOsioChico@users.noreply.github.com>
2023-06-09 21:53:28 -0500

    build: Add ESLint, Prettier and TypeScript configs

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Crew App</h1>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@components': '/src/components',
+    },
+  },
+})
